8297718ac3808fb964e80b7f57c2d09b60453ab5
Shan-A0 <shan.a_claire@hotmail.com>
2015-12-18 21:47:02 -0500

    Update CheapoNewMess.php

--- CheapoNewMess.php
+++ CheapoNewMess.php
@@ -1,6 +1,6 @@
 <?php 
   session_start(); 
-  //include 'login.php'; 
+  //include 'CheapoLogin.php'; 
   $recepient = $_POST["recipient"]; 
   $subject = $_POST["subject"]; 
   $body = $_POST["body"]; 
@@ -12,13 +12,13 @@
   if(isset($_SESSION['username'])){ 
       $useridquery =  "SELECT id FROM User WHERE name = '$_SESSION[username]'; "; 
       $recidquery =  "SELECT id FROM User WHERE name = '$recipient'; "; 
-      $userres = mysqli_query($con,$useridquery); 
-      $recres = mysqli_query($con,$recidquery); 
-      if(mysql_fetch_array($recres)==0){ 
+      $userresponse = mysqli_query($con,$useridquery); 
+      $recresponse = mysqli_query($con,$recidquery); 
+      if(mysql_fetch_array($recresponse)==0){ 
           echo "Invalid Recipient Username"; 
       }else{ 
-          while($row=mysql_fetch_array($userres)){ 
-             while($row2=mysql_fetch_array($recres)){ 
+          while($row=mysql_fetch_array($userresponse)){ 
+             while($row2=mysql_fetch_array($recresponse)){ 
                  $sql = "INSERT INTO message (body,subject,user_id,recipient_id) VALUES ($body,$subject,'$row[id]','$row2[id]');"; 
                  if (!mysqli_query($con,$sql)){ 
        					    die('Error: ' . mysqli_error($con)); 
