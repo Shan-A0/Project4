35f747571274a87de4d101a519d9209bd1c77525
Shan-A0 <shan.a_claire@hotmail.com>
2015-12-18 21:30:55 -0500

    Update CheapoLogin.js

--- CheapoLogin.js
+++ CheapoLogin.js
@@ -3,23 +3,23 @@ window.onload = function(){
 function verify(){ 
 	var username = $("username").value; 
  	var password = $("password").value; 
-  var re =/((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,})/ ; 
+  	var re =/((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,})/ ; 
 	if(!(re.test(password))){ 
-     alert("Password not valid"); 
-     return false; 
+	alert("Password not valid"); 
+     	return false; 
 	} 
   var responseMessage; 
-	var requeststring = "login.php?username="+username+"&password="+password; 
+	var requeststring = "CheapoLogin.php?username="+username+"&password="+password; 
 	var getstuff = new XMLHttpRequest(); 
 	getstuff.onreadystatechange = function(){ 
-       if(getstuff.readyState==4 && getstuff.status==200 ){ 
+       if(getstuff.readyState==8 && getstuff.status==200 ){ 
           responseMessage = getstuff.responseText; 
           console.log(responseMessage); 
           if (responseMessage=="fail") { 
               document.getElementById("login_status").innerHTML= "<div class='loginstat'><strong> Login Failed </strong></div>"; 
          }else if(responseMessage=="pass"){ 
              document.getElementById("login_status").innerHTML= "<div class='loginstat'><strong> Login Success </strong></div>"; 
-             window.location.href="homepage.html"; 
+             window.location.href="CheapoHome.html"; 
          } 
       }
  	}; 
