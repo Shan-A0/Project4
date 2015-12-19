efc701a9b5a87c21a53f9919f6637b7151d2b7dd
Shan-A0 <shan.a_claire@hotmail.com>
2015-12-18 22:23:14 -0500

    Create CheapoAdduser.php

---
+++ CheapoAdduser.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+    <title> CheapoMail: Add User</title> 
+    <link rel=stylesheet type="text/css" href="CheapoLogin.css"> 
+    <script type="text/javascript" src="CheapoLogin.js"></script> 
+</head> 
+<body> 
+    <form id="logininfo" method="post">  
+    <fieldset> 
+    <div id="info"> 
+       <em>Username</em><br> <input type="text" id ="username" name="username" class="textfield"> <br> 
+       <em>Password</em><br> <input type="password" id ="password" name="password" class="textfield"> <br> 
+       <em>First Name</em><br> <input type="text" id ="firstname" name="firstname" class="textfield"> <br> 
+       <em>Last Name</em><br> <input type="text" id ="lastname" name="lastname" class="textfield"> <br> 
+       <button type= "submit" id="login" > <strong> ADD USER </strong> </button> 
+    </div> 
+    </fieldset> 
+    </form> 
+    <div id= "login_status"></div> 
+</body> 
+</html> 
